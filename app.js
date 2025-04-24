336423adbcfb3ed8b941d117858bc20c6b54787d
mw613 <mw613@students.waikato.ac.nz>
2025-04-24 13:28:33 +1200

    added showing search list (nonfunctional)

--- app.js
+++ app.js
@@ -17,9 +17,41 @@ httpServer.listen(PORT, () => { // start http server listening
     console.log('httpServer running at http://localhost:' + PORT);
 });
 
+const usersFinding = ['test1','test2','test3'];
 io.on('connection', (socket) => { // start socket server listening
     console.log('connection made!');
+    let username = null;
+    socket.on('find', (newUsername) => {
+        console.log('finding for user: ' + newUsername)
+        if (!isValidUsername(newUsername)) {
+            socket.emit('error', 'invalid username, 3-16 letters with no whitespaces only');
+            return;
+        }
+        if (usersFinding.includes(newUsername)) {
+            socket.emit('error', 'there is already a user called that, try another name.');
+            return;
+        }
+        username = newUsername;
+        usersFinding.push(username);
+        socket.emit('find-results', usersFinding);
+    });
+    socket.on('find-again', () => {
+        socket.emit('find-results', usersFinding);
+    });
+
     socket.on('disconnect', function() {
         console.log('disconnect');
-    })
+        if (username != null) {
+            if (usersFinding.includes(username)) {
+                console.log(username + ' is removed from usersFinding')
+                usersFinding.splice(usersFinding.indexOf(username), 1);
+                // TODO: update currently finding players
+                // LATER: put all users finding into a room
+            }
+        }
+    });
 });
+
+function isValidUsername(username) {
+    return typeof(username) == 'string' && username.match('^[a-zA-Z]+') && 2 < username.length < 16;
+}
--- client/script.js
+++ client/script.js
@@ -1,7 +1,42 @@
 const socket = io();
+const startmenu = document.getElementById('start-menu');
+const findmenu = document.getElementById('find-menu');
+const findListContainer = document.getElementById('find-list');
+let username = null;
 
 socket.on('connect', function() {
     console.log('connected');
+});
+
+function find() {
+    username = prompt('enter user name:');
+    socket.emit('find', username);
+    startmenu.classList.add('hidden');
+    findmenu.classList.remove('hidden');
+}
+socket.on('error', (message) => {
+    alert(message);
+})
+socket.on('find-results', (usersFinding) => {
+    console.log('find results are: ', usersFinding);
+    for (let findUsername of usersFinding) {
+        if (findUsername == username) { // dont show self
+            continue;
+        }
+        const div = document.createElement('div');
+        div.innerText = findUsername;
+        div.id = findUsername + '-list-item';
+        div.classList.add('find-result');
+        findListContainer.appendChild(div);
+
+        const requestButton = document.createElement('button');
+        requestButton.classList.add('request-game-button');
+        requestButton.innerText = 'Request';
+        requestButton.addEventListener('onclick', () => {
+            socket.emit('request-game', findUsername);
+        });
+        div.appendChild(requestButton);
+    }
 });
 
 socket.on('disconnect', function() {
