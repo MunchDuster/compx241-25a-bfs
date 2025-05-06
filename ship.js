85dc80ced42a8177490339ec1cdb053c6b5336db
Troy McCulloch <tm1005@students.waikato.ac.nz>
2025-05-06 22:58:34 +1200

    Can get Centre Tile and Tile Array from Ship Class (probably)

--- ship.js
+++ ship.js
@@ -7,26 +7,26 @@ const userShips = new Map();
 
 function Ship(type,centreTile) {
     this.type = type; //Is Ship a battleship,cruiser,carrier,destroyer, or submarine
-    this.centreTile = centreTile; //Middle position of Ship (Anchored Mouse Position).
+    this.centreTile = centreTile; //Middle position of Ship (Anchored Mouse Position). centreTile = {x : 0, y: 0}
     this.numOfTiles = null; //Total amount of tiles it will occupy
     this.tileArray = null; //All detailed positions of Tiles it occupies
 
     this.numOfTiles = function() {
         switch(this.type) {
             case carrier:
-                this.numOfTiles = 5
+                this.numOfTiles = 5;
                 break;
             case battleship:
-                this.numOfTiles = 4
+                this.numOfTiles = 4;
                 break;
             case cruiser:
-                this.numOfTiles = 3
+                this.numOfTiles = 3;
                 break;
             case submarine:
-                this.numOfTiles = 3
+                this.numOfTiles = 3;
                 break;
             case destroyer:
-                this.numOfTiles = 2
+                this.numOfTiles = 2;
                 break;
             default:
                 console.error('Different Case found for this.numOfTiles in ship.js');
@@ -35,11 +35,67 @@ function Ship(type,centreTile) {
         this.tileArray = new Array(this.numOfTiles);
     };
 
+    this.tileArray = function() {
+        centreX = centreTile.x;
+        centreY = centreTile.y;
+        switch(this.numOfTiles) {
+            case 5:
+                this.tileArray = 
+                [{x : centreX-2, y : centreY},{x : centreX-1, y : centreY},
+                {x : centreX, y : centreY},
+                {x : centreX+1, y : centreY},{x : centreX+2, y : centreY}];
+                break;
+            case 4:
+                this.tileArray = 
+                [{x : centreX-1, y : centreY},
+                {x : centreX, y : centreY},
+                {x : centreX+1, y : centreY},{x : centreX+2, y : centreY}];
+                break;
+            case 3:
+                this.tileArray = 
+                [{x : centreX-1, y : centreY},
+                {x : centreX, y : centreY},
+                {x : centreX+1, y : centreY}];
+            case 2:
+                this.tileArray = 
+                [{x : centreX, y : centreY},{x : centreX+1, y : centreY}];
+                break;
+            default:
+                console.error('Different Case found for this.tileArray in ship.js');
+        }
+    }
+
     this.toString = function() {
         return (this.type ?? '[no_shipType]') + '@' + this.centreTile.x + this.centreTile.y;
     };
 
-    userShips.set(type, centreTile);
+    userShips.set(this.tileArray, this.type);
+}
+
+Ship.getTilePositions = function() {
+    for([tileArray, type] of userShips) {
+        if(tileArray == this.tileArray) {
+            return this.tileArray;
+        }
+    }
+    return null; //Not found.
+}
+
+Ship.getCentreTile = function() {
+    for([tileArray, type] of userShips) {
+        if(tileArray == this.tileArray && type == this.type) {
+            switch(this.numOfTiles){
+                case 5:
+                    return tileArray[2];
+                case 4:
+                case 3:
+                    return tileArray[1]
+                case 2:
+                    return tileArray[0]
+            }
+        }
+    }
+    return null; //Not found.
 }
 
 module.exports = Ship;
