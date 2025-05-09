74b0f8c33256078628e9825afa633bae87be786a
19maxwill <19maxwill@gmail.com>
2025-05-09 16:36:17 +1200

    Konva prototype  (please help fix import)

--- client/drawer.js
+++ client/drawer.js
@@ -1,33 +1,31 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-const colors = {
-    bg: '#4279aa',
-    line: 'black'
-}
+import Konva from 'konva';
 
 const width = canvas.width;
 const height = canvas.height;
 console.log(width + ' ' + height);
 const gridSize = 10; // same as normal battleships
-const offset = 4; //offset for squares to get grid lines inbetween squares for aesthetics
-const squares = [];
+const offset = 4; //offset for tiles to get grid lines inbetween tiles for aesthetics.
+const size = Math.round(height / gridSize) - offset * 2;
+const tiles = [];
 
-class Rectangle {
-    constructor(x, y, id, color) {
-        this.x = x;
-        this.y = y;
-        this.width = Math.round(height / gridSize) - offset * 2;
-        this.height = Math.round(height / gridSize) - offset * 2;
-        this.color = color;
-        this.id = id; //id is x-row, y-row, grid e.g a square in the first row, second column in the grid on the right is 122
-    }
+// class Tile {
+//     constructor(x, y, id, color) {
+//         this.x = x;
+//         this.y = y;
+//         this.width = 
+//         this.height = Math.round(height / gridSize) - offset * 2;
+//         this.color = color;
+//         this.id = id; //id is x-row, y-row, grid e.g a square in the first row, second column in the grid on the right is 122
+//     }
 
-    draw(ctx) {
-         ctx.fillStyle = this.color;
-         ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-}
+//     draw(ctx) {
+//         ctx.fillStyle = this.color;
+//         ctx.fillRect(this.x, this.y, this.width, this.height);
+//     }
+// }
 
 
 const deltaYPos = height / (gridSize + 1); // + 1 for gap-to-edge, + 1 for side gaps
@@ -35,21 +33,30 @@ const deltaXPos = Math.min(height / (gridSize + 1), deltaYPos);
 
 console.log(deltaXPos + ' ' + deltaYPos);
 
-let isDrawing = false;
 ctx.imageSmoothingEnabled = false;
 
 
 // (x,y) is a grid point
 // (xPos, yPos) is a canvas pixel
 function startDrawing() {
+    console.log('Starting to draw game board...');
     draw();
 }
 
 function draw() {
+    console.log('Drawing tiles...');
     clear();
-    makeSquares(0, 1);
-    makeSquares(width/2, 2);
-    squares.forEach(Rectangle => Rectangle.draw(ctx));
+    makeTiles(0, 1);
+    makeTiles(width/2, 2);
+    console.log(`Created ${tiles.length} tiles`);
+
+    const stage = new Konva.Stage({
+      container: 'canvas', width: width, height: height
+    });
+    const layer = new Konva.Layer();
+
+    tiles.forEach(tile => layer.add(tile));
+    stage.add(layer);
 }
 
 
@@ -57,12 +64,16 @@ function clear() {
     ctx.clearRect(0, 0, width, height);
 }
 
-function makeSquares(offsetX, gridNum) { 
+function makeTiles(offsetX, gridNum) { 
     for (let y = 0; y < gridSize; y++) {
         for (let x = 0; x < gridSize; x++) {
             const xPos = getGridStartYPos(x) + offsetX + offset;
             const yPos = getGridStartYPos(y) + offset;
-            squares.push(new Rectangle(xPos, yPos, x.toString() + y.toString() + gridNum.toString(), "#5F85B5"));
+           // tiles.push(new Tile(xPos, yPos, x.toString() + y.toString() + gridNum.toString(), "#5F85B5"));
+             const rect1 = new Konva.Rect({
+             x: xPos, y: yPos, width: size, height: size, fill: 'red'
+            });
+            shapes.push(rect1);
         }     
     }
 }
@@ -75,14 +86,14 @@ function getGridStartYPos(y) {
     return Math.round((deltaYPos / 2.0) + y * deltaYPos);
 }
 function getGridPos({x, y}) {
-    return {x: getGridStartXPos(x), y: getGridStartYPos(y)}
+    return {x: getGridStartXPos(x), y: getGridStartYPos(y)};
 }
 
 // startDrawing(); // for debugging
 
 function isIntersect(p, r) {
     return (p.x >= r.x && p.x < (r.x + r.width) && p.y >= r.y && p.y < (r.y + r.height));
-  }
+}
 
 canvas.addEventListener('click', (e) => {
     var rect = canvas.getBoundingClientRect();
@@ -90,10 +101,14 @@ canvas.addEventListener('click', (e) => {
       x: e.clientX - rect.left,
       y: e.clientY - rect.top
     };
-    squares.forEach(Rectangle => {
-      if (isIntersect(pos, Rectangle)) {
-        Rectangle.color = "green";
-        Rectangle.draw(ctx);
+    tiles.forEach(Tile => {
+      if (isIntersect(pos, Tile)) {
+        Tile.color = "green";
+        Tile.draw(ctx);
       }
     });
-  });
+  }
+);
+
+window.startDrawing = startDrawing;
+window.clear = clear;
