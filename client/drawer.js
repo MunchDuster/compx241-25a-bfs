f7e74bd0689d41989a591a427bcd28c733d50e43
Sheng Ting Lim <sl774@students.waikato.ac.nz>
2025-05-13 15:36:40 +1200

    fix: ship placement import and drawing

--- client/drawer.js
+++ client/drawer.js
@@ -83,6 +83,62 @@ function makeTiles(offsetX, gridNum) {
     }
 }
 
+function startPlacingShips() {
+    console.log('Adding Ships...');
+	layer = new Konva.Layer();
+	stage.add(layer);
+    addShips();
+}
+
+const shipTypes = ["carrier", "battleship", "cruiser", "submarine", "destroyer"];
+
+function addShips() {
+
+    for (let i = 0; i < shipTypes.length; i++) { //repeat for all ship types
+
+        //get ship sizes for referencing img sizes
+        let size = 0;
+        switch(shipTypes[i]) {
+            case 'carrier':
+                size = 5;
+                break;
+            case 'battleship':
+                size = 4;
+                break;
+            case 'crusier':
+            case 'submarine':
+                size = 3;
+                break;
+            case 'destroyer':
+                size = 2;
+                break;
+            default:
+                size = 0;
+        }
+
+        const shipImg = new Image();
+        shipImg.onload = function () {
+            const ship = new Konva.Image({
+                x: 500,
+                y: i * 10,
+                image: shipImg,
+                width: 50,
+                height: 50 * size
+            });
+
+            layer.add(ship);
+
+             ship.on('click', function(e) {
+                this.opacity(0.5); //set the opacity of the ship to 50% when clicked on
+            });
+
+        };
+       
+        let shipPath = `../assets/${shipTypes[i]}.png`; //set ship image to correct ship
+        shipImg.src = shipPath;
+    }  
+}    
+
 function getGridStartXPos(x) {
     return Math.round((deltaXPos / 2.0) + x * deltaXPos);
 }
@@ -93,3 +149,4 @@ function getGridStartYPos(y) {
 
 window.startDrawing = startDrawing;
 window.clear = clear;
+window.startPlacingShips = startPlacingShips;
