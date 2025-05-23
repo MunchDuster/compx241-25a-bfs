3bc056654cda161a31a0c4e75311c7b926a10e71
19maxwill <19maxwill@gmail.com>
2025-05-23 20:16:47 +1200

    mouse wheel rotation works but r key has bug that I can't figure out why - repeats 5 times when it should only repeat once per key press

--- client/drawer.js
+++ client/drawer.js
@@ -92,8 +92,6 @@ function makeTiles(offsetX, gridNum) {
                             //move the ship to selected tile
                             ship.x(this.x()); 
                             ship.y(this.y());
-                            //aline ship when moving to selected tile
-                            ship.offsetY((ship.height() / 2) - (size / 2));
 
                             //unselect ship
                             ship.opacity(1);
@@ -148,12 +146,13 @@ function addShips() {
         const shipImg = new Image();
         shipImg.onload = function () {
             const ship = new Konva.Image({
-                x: tiles[i].x(),
-                y: tiles[i].y(),
+                x: tiles[i + 20].x(),
+                y: tiles[i + 20].y(),
                 image: shipImg,
                 width: size,
                 height: (size + offset) * shipSize - offset, //math to get ship to fit within tiles
-                id: `${shipTypes[i]}-${1}`, //id storing ship type plus rotation (1 for up and down, 2 for left and right)
+                id: `${shipTypes[i]} Up`, //id storing ship type plus rotation (1 for up and down, 2 for left and right)
+                offsetY: (((size + offset) * shipSize - offset)/2) - (size / 2), //set initial offset
             });
             console.log(`Ship (${shipTypes[i]}) placed at position (${tiles[i].id()})`);
             layer.add(ship);
@@ -163,46 +162,127 @@ function addShips() {
                 ships.forEach(ship => { //unselect the other ships to prevent multiple ships from being selected
                     ship.opacity(1);
                 })
-                this.opacity(this.opacity() === 1 ? 0.5 : 1); //set the opacity of the ship to 50% when clicked on
+                this.opacity(0.5); //set the opacity of the ship to 50% when clicked on
                 console.log(`Selected ${this.id()}`);
             });
 
             ship.on('wheel', function() {
-                if(ship.opacity() == 0.5) {
+                if(this.opacity() == 0.5) {
                     //check the id (makes it easier to get ship rotation)
-                    if (this.id().split(' ')[1] == 1){ //rotate vertically
-                        this.id(`${shipTypes[i]} ${2}`);
-                        this.offsetY((this.height() / 2) - (size / 2));
-                        this.offsetX(0);
-                        this.rotate(90); //rotate ship 90 degrees 
-                        console.log("up");
+                    switch (this.id().split(' ')[1]) {
+                        case 'Up':
+                            //rotate the ship right
+                            this.id(`${shipTypes[i]} Right`);
+                            this.rotate(90);
+                            if(shipTypes[i] == "destroyer" || shipTypes[i] == "battleship") {
+                                this.offsetY((this.height() / 2) + size);
+                            } else { this.offsetY((this.height() / 2) + (size / 2)); }
+                            this.offsetX(0);
+                            break;
 
-                    } else { //rotate horizontally
-                        this.id(`${shipTypes[i]} ${1}`);
-                        this.offsetX(size);
-                        this.rotate(-90);
-                        console.log("left");
-                    } 
+                        case 'Right':
+                            //rotate the ship downwards
+                            this.id(`${shipTypes[i]} Down`);
+                            this.rotate(90);
+                            this.offsetX(size);
+                            if(shipTypes[i] == "destroyer" || shipTypes[i] == "battleship") {
+                                this.offsetY((this.height() / 2) + size);
+                            } else { this.offsetY((this.height() / 2) + (size / 2)); }
+                            break;
+
+                        case 'Down':
+                            //rotate the ship left
+                            this.id(`${shipTypes[i]} Left`);
+                            this.rotate(90);
+                            this.offsetX(size);
+                            if(shipTypes[i] == "destroyer" || shipTypes[i] == "battleship") {
+                                this.offsetY((this.height() / 2));
+                            } else { this.offsetY((this.height() / 2) - (size / 2)); }
+                            break;
+
+                        case 'Left':
+                            //rotate the ship upwards
+                            this.id(`${shipTypes[i]} Up`);
+                            this.rotate(90);
+                            this.offsetX(0);
+                            if(shipTypes[i] == "destroyer" || shipTypes[i] == "battleship")  { 
+                                this.offsetY((this.height() / 2));
+                            } else { this.offsetY((this.height() / 2) - (size / 2)); }
+                            break;
+                    }
                 }
             })
 
             document.addEventListener('keydown', (e) => {
                 if (e.key.toLowerCase() === 'r') {
-                    const selectedShip = ships.find(ship => ship.opacity() === 0.5);
-                    if (selectedShip) {
-                        if (selectedShip.id().split(' ')[1] == 1){ //rotate vertically
-                            selectedShip.id(`${shipTypes[i]} ${2}`);
-                            selectedShip.offsetY((selectedShip.height() / 2) - (size / 2));
-                            selectedShip.offsetX(0);
-                            selectedShip.rotate(90); //rotate ship 90 degrees 
-                            console.log("up");
-                        } else { //rotate horizontally
-                            selectedShip.id(`${shipTypes[i]} ${1}`);
-                            selectedShip.offsetX(size);
-                            selectedShip.rotate(-90);
-                            console.log("left");
-                        } 
-                    }
+
+                    ships.forEach(ship => {
+                        if(ship.opacity() == 0.5) {
+                            console.log("selected" );
+                        } else { console.log("not selected " + ship.id()); }
+                        });
+                            // //get selected ship and id info
+                            // const shipOrient = ship.id().split(' ')[1];
+                            // const shipType = !(ship.id().split(' ')[0] != "destroyer" && ship.id().split(' ')[0] != "battleship");
+
+                            // switch (shipOrient) {
+                            //     case 'Up':
+                            //         //rotate the ship right
+                            //         ship.id(`${shipTypes[i]} Right`);
+                            //         ship.rotate(90);
+                            //         ship.offsetX(0);
+                            //         if(shipType) {
+                            //             ship.offsetY((ship.height() / 2) + size);
+                            //             console.log("even");
+                            //         } else { 
+                            //             ship.offsetY((ship.height() / 2) + (size / 2));
+                            //             console.log("odd");
+                            //         }
+                            //     break;
+
+                            //     case 'Right':
+                            //         //rotate the ship downwards
+                            //         ship.id(`${shipTypes[i]} Down`);
+                            //         ship.rotate(90);
+                            //         ship.offsetX(size);
+                            //         if(shipType) {
+                            //             ship.offsetY((ship.height() / 2) + size);
+                            //             console.log("even");
+                            //         } else { 
+                            //             ship.offsetY((ship.height() / 2) + (size / 2)); 
+                            //             console.log("odd");
+                            //         }
+                            //     break;
+
+                            //     case 'Down':
+                            //         //rotate the ship left
+                            //         ship.id(`${shipTypes[i]} Left`);
+                            //         ship.rotate(90);
+                            //         ship.offsetX(size);
+                            //         if(shipType) {
+                            //             ship.offsetY((ship.height() / 2));
+                            //             console.log("even");
+                            //         } else { 
+                            //             ship.offsetY((ship.height() / 2) - (size / 2)); 
+                            //             console.log("odd");
+                            //         }
+                            //     break;
+
+                            //     case 'Left':
+                            //         //rotate the ship upwards
+                            //         ship.id(`${shipTypes[i]} Up`);
+                            //         ship.rotate(90);
+                            //         ship.offsetX(0);
+                            //         if(shipType)  { 
+                            //             ship.offsetY((ship.height() / 2));
+                            //             console.log("even");
+                            //         } else { 
+                            //             ship.offsetY((ship.height() / 2) - (size / 2)); 
+                            //             console.log("odd");
+                            //         }
+                            //     break;
+                            // }
+                    
                 }
             }
         );        
