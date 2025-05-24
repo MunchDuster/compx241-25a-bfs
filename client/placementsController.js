f0b0b7540758316518527c0d1d2e252906b45153
Sheng Ting Lim <shenglim701@gmail.com>
2025-05-25 00:14:45 +1200

    fix: reset rotation of ship if let go and ship is not placed down and so ship is set back the the docks straightly!!!!!!!

--- client/placementsController.js
+++ client/placementsController.js
@@ -331,6 +331,7 @@ function placeShip(konvaShip) {
     }
 
     if (!shipPlaced) {
+        konvaShip.rotation(0);
         konvaShip.position({ x: ship.dockX, y: ship.dockY });
         konvaShip.moveTo(stagesAndLayers.shipPlacementLayer);
         ship.isPlaced = false;
