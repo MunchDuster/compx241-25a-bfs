474f9d8b430efeee2ab5b0c4959fca659ea4f670
EatableTuna <devo-1996@hotmail.com>
2025-06-02 17:26:51 +1200

    Fix: Seperated showing and hiding of username alert into two seperate functions.

--- client/script.js
+++ client/script.js
@@ -62,7 +62,7 @@ socket.on('error', (message) => {
     //Super clunky I know
     if(message === 'Invalid username. Must be 3-16 letters with no spaces.') {
         //'Invalid username. Must be 3-16 letters with no spaces.'
-        clopen();
+        showUsernameAlert();
     } else {
         alert(message);
     }
@@ -235,16 +235,14 @@ function getGameState() {
 }
 
 window.getGameState = getGameState;
-
-function clopen() {
-    
-    let label = document.getElementById('invalid-username-alert');
-    if(label.classList.contains('hidden')) {
-        label.classList.remove('hidden');
-    } else {
-        label.classList.add('hidden');
-    }
+function showUsernameAlert() {
+    document.getElementById('invalid-username-alert').classList.remove('hidden');
 }
+// 
+function closeUsernameAlert() {
+    document.getElementById('invalid-username-alert').classList.add('hidden');
+}
+
 // Handle user clicking "Find Game" button
 function find() {
     // Prompt user to input username
